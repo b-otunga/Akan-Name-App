34d9e4928139fe8b3f36c4e23415ca8cf8fbd1d4
Bill <billotungah@gmail.com>
2022-09-28 14:52:45 +0300

    add results button

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,2 +1,5 @@
-const DAYS_OF_THE_WEEK = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-const MALE_NAMES = []
+const DAYS_OF_THE_WEEK = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const MALE_NAMES = [];
+const FEMALE_NAMES = [];
+
+var gender = 
