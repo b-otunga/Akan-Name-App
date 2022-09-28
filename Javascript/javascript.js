b0380631ea6949b1fd06108d71bd37beb773e820
Bill <billotungah@gmail.com>
2022-09-28 23:33:55 +0300

    add event listener to date picker

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,17 +1,25 @@
 const DAYS_OF_THE_WEEK = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 const MALE_NAMES = [];
 const FEMALE_NAMES = [];
-
-
-var date = document.getElementById("Date").value;
-
 var name = null
 
-function getUserInput()
-{var gender = prompt(select your gender)
-var century = prompt(select your gender)
-var year = prompt(select your gender)
+let date = document.querySelector("#Date");
+console.log(date);
+
+// fetch date figure from user section in string form
+date.valueAsDate = new Date();
+
+date.onchange = function(){
+   console.log(date.value);}
+
+// fetch user's gender selection
+
+let gender = document.querySelector(".dropdown-menu");
+
+gender.addEventListener("change", () => {XPathResult.innerText = selection.options[selection.selectedIndex].text});
 
 
-}
+console.log(gender)
+
+
 
