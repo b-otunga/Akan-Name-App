baece6b6d3163e9c08d13117e974a1f0e9c07cc6
Bill <billotungah@gmail.com>
2022-09-29 21:17:27 +0300

    add function for calculating day

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,48 +1,85 @@
-const DAYS_OF_THE_WEEK = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-const SWA_NAMES = ["Jumatatu", "Jumanne", "Jumatano", "Alhamisi", "Ijumaa", "Jumamosi", "Jumapili"];
-const SHORT_NAMES = ["Mon", "Tue", "Wed", "Thurs", "Fri", "Sato", "Sun"];
-var akhaiName
-var gender
+const DAYS_OF_THE_WEEK = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const MALE_NAMES = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const FEMALE_NAMES = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var akanName;
+var gender;
+var datenow;
+var y
 
-let date = document.querySelector("#Date");
-console.log(date);
+// FETCH DATE FROM INPUT
+function getDate () {
+    datenow = document.getElementById("Date").value;
+    // return datenow;
+    datenow = datenow.toString();
+    var CC = datenow.slice(0, 2);
+console.log(CC)
+CC = parseInt(CC);
+console.log(CC)
+var YY = datenow.slice (2, 4);
+YY = parseInt(YY);
+var MM = datenow.slice(5, 7);
+MM = parseInt(MM);
+var DD = datenow.slice(8, 10);
+DD= parseInt(DD);
 
-// fetch date figure from user section in string form
-date.valueAsDate = new Date();
+y = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
 
-date.onchange = function(){
-   console.log(date.value);}
+y = Math.floor(y);
 
-// fetch user's gender selection
+return datenow;
+  }
+
+
+
+// CALCULATE DAY OF BIRTH
+
+
+
+// // y = Math.round(y);
+
+// y = 4;
+
+// FETCH GENDER
 
 function getGenderValue () {
     gender = document.querySelector("#floatingSelect").value;
-    {console.log(gender);
+    console.log(gender);
     
-}
+
 return gender;
 }
-// determining day of birth
 
 
 
-// getting Akhai name
+// CREATE AKAN NAME
 
-function getAkhaiName(){
+function getAkanName(){
+    getDate ();
+    console.log(y);
+
     if (gender === "Male"){
-    indexOfDay = DAYS_OF_THE_WEEK.indexOf("Tuesday");
-      akhaiName = SWA_NAMES[indexOfDay];
-      console.log("Your Akhai Name is " + akhaiName);
+ 
+      akanName = MALE_NAMES[y];
+      console.log("Your Akan Name is " + akanName);
     }
     else if (gender === "Female"){
-     indexOfDay = DAYS_OF_THE_WEEK.indexOf("Saturday");
-        akhaiName = SHORT_NAMES[indexOfDay];
-       console.log("Your Akhai Name is " + akhaiName);}
-else{console.log("Please Pick Your Gender");
+
+        akanName = FEMALE_NAMES[y];
+       console.log("Your Akan Name is " + akanName);}
+else{alert("Please Pick Your Gender");
 
 }
-    }
 
+document.getElementById("result-button").innerHTML = "You were born on a " + DAYS_OF_THE_WEEK[y] + ". Your Akan name is " + akanName;
+}
+// console.log(CC);
+// // console.log(date.value.toString());
+// console.log(YY);
+// console.log(MM);
+// console.log(DD);
+// console.log(y);
+// console.log(datenow);
+  
 
 
 
